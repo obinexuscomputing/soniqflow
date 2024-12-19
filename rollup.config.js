a691ab85d553042fac9effe0348978234c929e3c
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-19 07:26:24 +0000

    init

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.umd.js',
+      format: 'umd',
+      name: 'SoniqFlow',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    })
+  ]
+};
