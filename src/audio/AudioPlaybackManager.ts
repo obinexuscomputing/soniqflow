46fc6c2c7f43adda347d15e693e00421c6f6095b
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-20 18:26:35 +0000

    init

--- src/audio/AudioPlaybackManager.ts
+++ src/audio/AudioPlaybackManager.ts
@@ -16,6 +16,9 @@ export class AudioPlaybackManager {
       document.addEventListener('keydown', resumeContext);
   }
 
+  public play(buffer: Float32Array, duration: number): void {
+      this.playBuffer(buffer, duration);
+  }
   public playBuffer(buffer: Float32Array, duration: number): void {
       const audioBuffer = this.context.createBuffer(1, buffer.length, this.context.sampleRate);
       audioBuffer.copyToChannel(buffer, 0);
@@ -26,4 +29,27 @@ export class AudioPlaybackManager {
       bufferSource.start();
       bufferSource.stop(this.context.currentTime + duration);
   }
+
+  static async loadAudioBuffer(url: string): Promise<Float32Array> {
+        const response = await fetch(url);
+        const arrayBuffer = await response.arrayBuffer();
+        const audioBuffer = await AudioPlaybackManager.decodeAudioData(arrayBuffer);
+        return audioBuffer.getChannelData(0);
+    }
+
+    private static async decodeAudioData(arrayBuffer: ArrayBuffer): Promise<AudioBuffer> {
+        const context = new AudioContext();
+        return new Promise((resolve, reject) => {
+            context.decodeAudioData(arrayBuffer, resolve, reject);
+        });
+    }
+
+    public async playAudioBufferFromURL(url: string, duration: number): Promise<void> {
+        const audioBuffer = await AudioPlaybackManager.loadAudioBuffer(url);
+        this.playBuffer(audioBuffer, duration);
+    }
+
+    stop(){
+        this.context.close();
+    }
 }
