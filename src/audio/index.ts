2df63b54ee22d8ccd52924ab5e3de1f7809423d2
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-20 12:49:02 +0000

    init

--- src/audio/index.ts
+++ src/audio/index.ts
@@ -2,5 +2,6 @@ export { AudioExporter } from './AudioExporter';
 export { AudioMixer } from './AudioMixer';
 export { AudioPlaybackManager } from './AudioPlaybackManager';
 export { AudioVisualizer } from './AudioVisualizer';
-export { AudioProcessor } from './AudioProcessor';
 export { AudioRecorder } from './AudioRecorder';
+export { AudioProcessor } from './AudioProcessor';
+export {AudioDriver} from './AudioDriver';
