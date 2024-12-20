077db2fdf37b3893585342aa2a90117582fa6684
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-12-20 15:33:34 +0000

    init

--- src/audio/AudioPlaybackManager.ts
+++ src/audio/AudioPlaybackManager.ts
@@ -1,22 +1,26 @@
+
 export class AudioPlaybackManager {
-    private audioContext: AudioContext;
-  
-    constructor() {
-      this.audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
-    }
-  
-    play(data: Float32Array): void {
-      const buffer = this.audioContext.createBuffer(1, data.length, this.audioContext.sampleRate);
-      buffer.copyToChannel(data, 0);
-  
-      const source = this.audioContext.createBufferSource();
-      source.buffer = buffer;
-      source.connect(this.audioContext.destination);
-      source.start();
-    }
-  
-    stop(): void {
-      this.audioContext.close();
+  private audioContext: AudioContext;
+
+  constructor() {
+    this.audioContext = new (window.AudioContext || (window as any).webkitAudioContext)();
+  }
+
+  play(data: Float32Array, bubble: boolean = true): void {
+    const buffer = this.audioContext.createBuffer(1, data.length, this.audioContext.sampleRate);
+    buffer.copyToChannel(data, 0);
+
+    const source = this.audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(this.audioContext.destination);
+    source.start();
+
+    if (bubble) {
+        console.log('Audio playback bubbled to connected nodes.');
     }
   }
-  
+
+  stop(): void {
+    this.audioContext.close();
+  }
+}
